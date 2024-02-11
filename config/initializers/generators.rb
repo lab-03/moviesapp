55b2b85d710c5ee205c9c253f8ef7cafb8810a72
Sadat97 <moanwar1997@gmail.com>
2024-02-11 13:39:29 +0200

    Install and configure factory_bot_rails gem
    - Install factory_bot_rails
    - Include factory_bot methods in tests
    - Replace fixtures with factories in generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,6 @@
 Rails.application.config.generators do |g|
+# Generate "users_factory.rb" instead of "users.rb"
+g.factory_bot suffix: "factory"
   # Disable generators we don't need.
   g.javascripts false
   g.stylesheets false
